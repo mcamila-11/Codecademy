a2a5ee52e3ee5263d39d41acbcc62e5f648e9af8
Camila Hernandez <mcamila11h@gmail.com>
2016-11-27 20:01:08 -0500

    Create 2parameters.js

---
+++ 2parameters.js
@@ -0,0 +1,13 @@
+// Learn JavaScript
+// Functions Lesson
+// Passing in parameters to a function
+// Call the function 3 times and pass in different arguments
+
+
+function takeOrder(topping, crustType){
+  console.log('Order: ' + crustType + ' pizza topped with ' + topping);
+}
+
+takeOrder('bacon', 'thin crust');
+takeOrder('pineapple', 'thick crust');
+takeOrder('pepperoni', 'cheesy crust');
